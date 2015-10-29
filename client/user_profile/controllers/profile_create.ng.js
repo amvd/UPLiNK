c0b6cee5d0b0adb3182744dcf36e905e3aadfc60
Armand <adeasis10@gmail.com>
2015-10-29 02:20:34 -0700

    misc bugfixes and refinements

--- client/user_profile/controllers/profile_create.ng.js
+++ client/user_profile/controllers/profile_create.ng.js
@@ -1,5 +1,7 @@
 angular.module("uplink").controller("profileCreateCtrl", function($scope, $meteor, $stateParams, $state){
 
+	// $scope.images = $meteor.collection(Images, false);
+
 	userId = $stateParams.userId;
 
 	$scope.user = Meteor.users.findOne({_id: $stateParams.userId});
@@ -12,7 +14,14 @@ angular.module("uplink").controller("profileCreateCtrl", function($scope, $meteo
 
 	$scope.saveUser = function(){
 		Meteor.users.update(userId, {$set:{profile: $scope.user.profile}});
+		// var file = $scope.pic_upload;
+		// $scope.images.insert(files[i], function(err, fileObj){
+		// 	console.log(fileObj);
+		// });
 		alert("User profile saved!");
 		$state.go("profile", {userId: $scope.user._id});
 	}
+
+
+
 });
